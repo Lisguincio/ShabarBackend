1c91a9b1e9d5188c1ebfc6f33c7e229263addd77
Mario Limone <redevil97@hotmail.it>
2022-09-06 13:05:04 +0200

    fixed sendmailer with gmail

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -5,8 +5,7 @@ const sendEmail = async (email, subject, emailTitle, emailBody) => {
   try {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: "Gmail",
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
